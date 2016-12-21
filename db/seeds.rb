4861bf687f54e8198eacfa78597e04c818fa47f7
jacqhernandez <jm.hernandez96@yahoo.com>
2016-12-21 16:04:46 -0300

    new

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,9 @@ Foodventure.create(date: Date.new(2018,11,5))
 Foodventure.create(date: Date.new(2019,11,5))
 Foodventure.create(date: Date.new(2020,11,5))
 Foodventure.create(date: Date.new(2021,11,5))
+Foodventure.create(date: Date.new(2021,11,5))
+Foodventure.create(date: Date.new(2021,11,5))
+Foodventure.create(date: Date.new(2021,11,5))
 
 FoodventureRestaurant.create(restaurant_id:4, foodventure_id:1) #your local
 FoodventureRestaurant.create(restaurant_id:3, foodventure_id:1) #mamou
@@ -42,6 +45,9 @@ FoodventureRestaurant.create(restaurant_id:1, foodventure_id:10)
 FoodventureRestaurant.create(restaurant_id:2, foodventure_id:11)
 FoodventureRestaurant.create(restaurant_id:6, foodventure_id:12)
 FoodventureRestaurant.create(restaurant_id:7, foodventure_id:13)
+FoodventureRestaurant.create(restaurant_id:7, foodventure_id:14)
+FoodventureRestaurant.create(restaurant_id:7, foodventure_id:15)
+FoodventureRestaurant.create(restaurant_id:7, foodventure_id:16)
 
 
 # Dish.create(foodventure_restaurant_id: 1, name: "Torched Salmon Donburi", price: 540, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "I will marry this", verdict: "pak", photo: File.new("#{Rails.root}/app/assets/images/salmon-small.jpg"), cuisine: "Japanese", course: "Main", meal: "Lunch, Dinner",tag:"salmon, fish")
@@ -54,7 +60,7 @@ Dish.create(foodventure_restaurant_id: 3, name: "Wildflour Breakfast", price: 24
 Dish.create(foodventure_restaurant_id: 3, name: "Tomato Soup", price: 240, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "Pig drumstick tri-tip kevin hamburger. Fatback bresaola prosciutto ham picanha. Pork loin pig pork belly kevin venison sausage chuck ham hock bresaola cupim frankfurter salami rump. Venison meatloaf boudin, jowl chuck sirloin meatball fatback filet mignon.", verdict: "pak", filename: "tomato-small.jpg", cuisine: "American", course: "Appetizer", meal: "Breakfast",tag:"tomato,soup")
 Dish.create(foodventure_restaurant_id: 3, name: "Wildflour Breakfast", price: 240, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "Pig drumstick tri-tip kevin hamburger. Fatback bresaola prosciutto ham picanha. Pork loin pig pork belly kevin venison sausage chuck ham hock bresaola cupim frankfurter salami rump. Venison meatloaf boudin, jowl chuck sirloin meatball fatback filet mignon.", verdict: "palpak", filename: "breakfast-small.jpg", cuisine: "American", course: "Main", meal: "Breakfast",tag:"potatoes")
 
-(3..15).each do |i|
+(3..18).each do |i|
   Dish.create(foodventure_restaurant_id: i, name: "Tofu Steak and Eggs", price: 370, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "Pig drumstick tri-tip kevin hamburger. Fatback bresaola prosciutto ham picanha. Pork loin pig pork belly kevin venison sausage chuck ham hock bresaola cupim frankfurter salami rump. Venison meatloaf boudin, jowl chuck sirloin meatball fatback filet mignon.", verdict: "palpak", filename: "tofu-small.jpg", cuisine: "American", course: "Main", meal: "Lunch, Dinner", tag:"tofu")
   Dish.create(foodventure_restaurant_id: i, name: "Angus Ribeye Steak", price: 1950, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "Pig drumstick tri-tip kevin hamburger. Fatback bresaola prosciutto ham picanha. Pork loin pig pork belly kevin venison sausage chuck ham hock bresaola cupim frankfurter salami rump. Venison meatloaf boudin, jowl chuck sirloin meatball fatback filet mignon.", verdict: "pak", filename: "steak-small.jpg", cuisine: "American", course: "Main", meal: "Lunch, Dinner",tag: "steak, ribeye")
   Dish.create(foodventure_restaurant_id: i, name: "Crispy Parmesan Churros", price: 240, comment_e: "Spicy jalapeno bacon ipsum dolor amet jowl t-bone pork belly tenderloin fatback. Kielbasa pork belly salami shank tenderloin kevin, t-bone sirloin venison leberkas boudin. Capicola salami meatball spare ribs, meatloaf flank pork chop frankfurter kielbasa prosciutto strip steak venison ribeye. ", comment_m: "Pig drumstick tri-tip kevin hamburger. Fatback bresaola prosciutto ham picanha. Pork loin pig pork belly kevin venison sausage chuck ham hock bresaola cupim frankfurter salami rump. Venison meatloaf boudin, jowl chuck sirloin meatball fatback filet mignon.", verdict: "pak", filename: "churros-small.jpg", cuisine: "Mexican", course: "Appetizer", meal: "Snacks",tag:"churros,parmesan,cheese")
